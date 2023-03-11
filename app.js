214fad4c02e7fc6f629dabdf82cb8bb959830e2f
Tshedza Mannye <luk3thebillionaire@gmail.com>
2023-03-11 22:31:17 +0200

    Add cookies

--- app.js
+++ app.js
@@ -3,47 +3,110 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
 const dbConnect = require('./config/dbConnection');
+const session = require('express-session');
+const  passport = require('passport');
+const passportLocal = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
 const app = express();
-dbConnect();
+const bcrypt = require('bcryptjs');
+const { use, authenticate, Passport } = require('passport');
+const salt = bcrypt.genSaltSync(10);
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 
+app.use(session({
+    secret:'ourSecret',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+dbConnect();
 const UserSChema = new mongoose.Schema({
     username: String,
     password: String
 })
 
-UserSChema.plugin(encrypt, {secret: process.env.SECRET_STRING, encryptedFields:['password']})
+
+UserSChema.plugin(passportLocalMongoose)
 
 const user = new mongoose.model('User',UserSChema)
+
+
+passport.use(new passportLocal((username, password, done)=>{
+    
+    user.findOne({username:username})
+    .then((foundUser)=>{
+        console.log("user object get by database",foundUser);
+        if (!foundUser || foundUser === null){
+            return done(null,false,{message: "There is no user with that ID"});
+        } 
+        if (!bcrypt.compareSync(password,foundUser.password)){
+            return done(null,false,{message:"Incorrect Password!"});
+        }
+        
+        return done(null,foundUser)
+        
+    })
+    .catch((err)=>{
+        return done(err)
+    });
+}
+));
+passport.serializeUser((user,done)=>{
+    console.log(user)
+    done(null,user._id)
+});
+
+passport.deserializeUser((id,done)=>{
+user.findById(id)
+    .then((foundUser)=>{
+        done(null,foundUser)
+    })
+    .catch((err)=>{
+        done(err);
+    })
+});
+
 app
     .get('/',(req,res)=>{
         res.render('home');
     });
 
-    
+app
+    .get('/secrets',(req,res)=>{
+        if (req.isAuthenticated()){
+            res.render('secrets')
+        }else{
+            res.redirect('/login')
+        }
+    })
 
 
 app
     .get('/login',(req,res)=>{
         res.render('login');
     })
-    .post('/login',async(req,res)=>{
-       await user.findOne({
-            username:req.body.username
-        })
-        .then((foundUser)=>{
-            if (foundUser.password===req.body.password){
-                res.render("secrets");
-            }
-        })
-        .catch((err)=>{
-            console.log(`User not found`)
-            console.log(err);
-        });
+    .post('/login',passport.authenticate('local',{
+        successRedirect:'/secrets',
+        failureRedirect :'/login'
+    }),(req,res)=>{
+        res.redirect('/')
+    //    user.findOne({
+    //         username:req.body.username
+    //     })
+    //     .then((foundUser)=>{
+    //         if (bcrypt.compareSync(req.body.password,foundUser.password)){
+    //             res.render("secrets");
+    //         }
+    //     })
+    //     .catch((err)=>{
+    //         console.log(`User not found`)
+    //         console.log(err);
+    //     });
     });
 
 
@@ -52,10 +115,10 @@ app
         res.render('register');
     })
     .post('/register',(req,res)=>{
-        
+        const hash= bcrypt.hashSync(req.body.password,salt)
         const newUser= new user ({
             username: req.body.username,
-            password: req.body.password
+            password: hash
         })
         
             
@@ -67,6 +130,12 @@ app
                 console.log(err)
             });
     });
+
+app.get('/logout',(req,res)=>{
+    req.logOut((done)=>{
+        res.redirect('/');
+    })
+})
     
 mongoose.connection.once('open',()=>{
     app.listen('3000',()=>{
